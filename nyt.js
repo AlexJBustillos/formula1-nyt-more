53597353d6c4e0332e78117f9f34b544516b166d
alexjbustillos <alexjbustillos@gmail.com>
2020-10-29 22:04:34 -0400

    [API & Other]practice makes perfect

--- nyt.js
+++ nyt.js
@@ -1,3 +1,16 @@
-console.log(credentials);
+console.log(API_KEY);
 
 // Write code here
+// fetch(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=big&opening-date=1980-01-01;1990-01-01&api-key=${API_KEY}`)
+// .then((res) => res.json()).then((json) => {
+//     console.log(json)
+// })
+
+async function deliverable1() {
+    console.log('I should log before data');
+    const res = await fetch(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=big&opening-date=1980-01-01;1990-01-01&api-key=${API_KEY}`)
+    const json = await res.json()
+    console.log(json)
+    console.log('I should be after data');
+}
+deliverable1()
--- other.js
+++ other.js
@@ -1,1 +1,32 @@
 // Write code here
+
+class Soccer {
+    constructor(player, team, country) {
+        this.player = player;
+        this.team = team;
+        this.country = country;
+        this.all = function () {
+            console.log(`The new player is ${this.player} and is on ${this.team} and is from ${this.country}`)
+        }
+    }
+    
+}
+const myNumber = new Soccer('Alex', 'NYCFC', 'USA');
+console.log(`The new player is ${myNumber.player}`);
+const karim = new Soccer('Karim Benzema', 'Real Madrid', 'France');
+console.log(`The new player is ${karim.player}`);
+const eden = new Soccer('Eden Hazard', 'Real Madrid', 'Belgium');
+console.log(`The new player is ${eden.player}`);
+Soccer.prototype.all = function () {
+    console.log('One more time');
+}
+eden.all();
+
+const morePromises = (condition) => new Promise((result, reject) => {
+    if (condition === 'Real Madrid') result('HALA MADRID')
+    reject('Pick a better team')
+})
+morePromises(karim.team).then(console.log).catch(console.error)
+morePromises().then(console.log).catch(console.error)
+
+
